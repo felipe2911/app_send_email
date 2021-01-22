217d658b889ce9480752481e2c92562841d37a7c
Felipe Carvalho <felipecarvalho2911@gmail.com>
2021-01-21 22:21:19 -0300

    app send email concluido

---
+++ app_send_email_publico/processa_envio.php
@@ -0,0 +1,9 @@
+<?php
+
+	require "../app_send_mail_privado/bibliotecas/PHPMailer/Exception.php";
+	require "../app_send_mail_privado/bibliotecas/PHPMailer/OAuth.php";
+	require "../app_send_mail_privado/bibliotecas/PHPMailer/PHPMailer.php";
+	require "../app_send_mail_privado/bibliotecas/PHPMailer/POP3.php";
+	require "../app_send_mail_privado/bibliotecas/PHPMailer/SMTP.php";
+
+	require "../app_send_mail_privado/processa_envio.php";
